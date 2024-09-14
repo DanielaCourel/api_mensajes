33be31bdf1cb247a8e270b601dbd3d13a3d5fb32
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-14 13:13:40 -0300

    corrección para permitir cualquier origen

--- swicher-back/app/main.py
+++ swicher-back/app/main.py
@@ -7,13 +7,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:8000",
-]
-
+# Configurando CORS para permitir peticiones desde el frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
