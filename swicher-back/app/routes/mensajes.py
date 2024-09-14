01dadddcb1be037c70c516ddd3a61906dc54c5e0
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-14 12:50:16 -0300

    correcciones en endpoints

--- swicher-back/app/routes/mensajes.py
+++ swicher-back/app/routes/mensajes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from app.database.models import Player, Mensaje
-from pony.orm import db_session, select
+from pony.orm import db_session, select, commit
 
 router = APIRouter()
 
@@ -9,21 +9,29 @@ async def mensajes():
     # Crear un objeto en la base de datos
     with db_session():
         mensajes = select(m for m in Mensaje)[:]
+        mensajes = [{"id": m.id, "mensaje": m.mensaje, "player": m.player.username, "id_player": m.player.id } for m in mensajes]
     return {"mensajes": mensajes}
+
 
 @router.post("/postear")
 async def publicar_mensaje(username: str, mensaje: str):
     with db_session():
-        player = Player.get(username=username)
+        player = select(p for p in Player if p.username == username).first()
         if not player:
             player = Player(username=username)
         Mensaje(mensaje=mensaje, player=player)
     return {"mensaje": mensaje, "player": username}
 
-@router.delete("/")
+
+@router.delete("/eliminarMensaje")
 async def borrar_mensajes(id: int, username: str):
     with db_session():
-        mensajes = select(m for m in Mensaje if m.id == id and m.player.username == username)[:]
-        if mensajes:
-            mensajes.delete()
-    return {"merequetengue"}
+        player = select(p for p in Player if p.username == username).first()
+        if player:
+            mensajes = select(m for m in Mensaje if m.id == id and m.player == player)[:]
+            for mensaje in mensajes:
+                mensaje.delete()
+                commit()
+                return {"mensaje": "Mensaje eliminado"}
+            return {"mensaje": "Mensaje no encontrado"}
+    return {"error": "Jugador no encontrado"}
--- swicher-back/app/routes/player.py
+++ swicher-back/app/routes/player.py
@@ -1,21 +1,21 @@
 from fastapi import APIRouter, status
 from app.database.models import Player
-from pony.orm import db_session
+from pony.orm import db_session, select
 from fastapi.responses import JSONResponse
 
 
 router = APIRouter()
 
-@router.get("/crear_player")
+@router.post("/crear_player")
 async def crear_player(username : str):
     with db_session():
         player = Player(username=username)
-    return JSONResponse(status_code=status.HTTP_201_CREATED)
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": "Player created successfully"})
 
 @router.delete("/borrar_player")
 async def borrar_player(username : str):
     with db_session():
-        player = Player.get(username=username)
+        player = select(p for p in Player if p.username == username).first()
         if player:
             player.delete()
-    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Player delete successfully"})
